2f49e528658d611631887a77d1aaad0f2cd7b20c
Deolin <splendid.deolin@gmail.com>
2018-05-28 20:52:14 +0800

    SqlTrimer支持输入目录

--- src/main/java/com/spldeolin/cadeau/support/client/SqlTrimer.java
+++ src/main/java/com/spldeolin/cadeau/support/client/SqlTrimer.java
@@ -2,6 +2,7 @@ package com.spldeolin.cadeau.support.client;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import org.apache.commons.io.FileUtils;
 import lombok.SneakyThrows;
 
@@ -9,13 +10,22 @@ public class SqlTrimer {
 
     @SneakyThrows
     public static void main(String[] args) {
+        File file = new File("");
+
         String regex = "AUTO_INCREMENT=[\\d]+ ";
-        File file = new File("C:\\Users\\deoli\\Desktop\\orderingtest_dubbo.sql");
-
-        String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-        content = content.replaceAll(regex, "");
-        FileUtils.write(file, content, StandardCharsets.UTF_8);
-
+        if (file.isDirectory()) {
+            Iterator<File> files = FileUtils.iterateFiles(file, new String[] {"sql"}, false);
+            while (files.hasNext()) {
+                File f = files.next();
+                String content = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
+                content = content.replaceAll(regex, "");
+                FileUtils.write(f, content, StandardCharsets.UTF_8);
+            }
+        } else {
+            String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+            content = content.replaceAll(regex, "");
+            FileUtils.write(file, content, StandardCharsets.UTF_8);
+        }
     }
 
 }
