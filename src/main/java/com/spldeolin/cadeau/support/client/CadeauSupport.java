3a573218aa0446bb9d37a2e38d53c668a775b11a
Deolin <splendid.deolin@gmail.com>
2018-06-03 15:33:41 +0800

    更新依赖版本

--- src/main/java/com/spldeolin/cadeau/support/client/CadeauSupport.java
+++ src/main/java/com/spldeolin/cadeau/support/client/CadeauSupport.java
@@ -1,6 +1,9 @@
 package com.spldeolin.cadeau.support.client;
 
 import com.spldeolin.cadeau.support.controller.ControllerGenerator;
+import com.spldeolin.cadeau.support.input.InputGenerator;
+import com.spldeolin.cadeau.support.persistence.MybatisGenerator;
+import com.spldeolin.cadeau.support.service.ServiceGenerator;
 import com.spldeolin.cadeau.support.util.ConfigUtil;
 
 /**
@@ -10,9 +13,9 @@ public class CadeauSupport {
 
     public static void main(String[] args) {
         ConfigUtil.assign();
-        //MybatisGenerator.daoMapperModel();
-        //InputGenerator.input();
-        //ServiceGenerator.serviceServiceImpl();
+        MybatisGenerator.daoMapperModel();
+        InputGenerator.input();
+        ServiceGenerator.serviceServiceImpl();
         ControllerGenerator.controller();
     }
 
