78345038c9538d6f7131356300afbddc0505af69
Deolin <splendid.deolin@gmail.com>
2018-08-01 13:39:25 +0800

    修正日期格式

--- src/main/java/com/spldeolin/cadeau/support/util/ProjectProperties.java
+++ src/main/java/com/spldeolin/cadeau/support/util/ProjectProperties.java
@@ -8,7 +8,7 @@ import static com.spldeolin.cadeau.support.util.ConstantUtils.sep;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Properties;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ArrayUtils;
@@ -181,7 +181,7 @@ public class ProjectProperties {
     // 最终路径 end
 
     private void initDefaultProperties() {
-        date = Times.toString(LocalDateTime.now());
+        date = Times.toString(LocalDate.now(), "yyyy/M/d");
         mapperPackage = "mapper";
     }
 
